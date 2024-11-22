dbb45663ef5bbc811c8dfb39c566a4057fb3a91f
timdeKuijper <timdekuijper@gmail.com>
2024-11-22 09:48:41 +0100

    microchip studio bestand toegevoegd

---
+++ robotcar/robotcar/main.c
@@ -0,0 +1,18 @@
+/*
+ * robotcar.c
+ *
+ * Created: 22-11-2024 09:46:49
+ * Author : timde
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
