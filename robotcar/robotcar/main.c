aaafab3e5abffe5eaf6bb2d596e8bd6eb4637069
Taco-san <143967870+Taco-san@users.noreply.github.com>
2024-11-29 11:35:19 +0100

    Motorcontrol toegevoegd

--- robotcar/robotcar/main.c
+++ robotcar/robotcar/main.c
@@ -18,6 +18,15 @@ int main(void){
     DDRD = 0x3C;
 	DDRC = 0x1;
 	DDRB = 0x32;
+	
+	// PWM setup
+	TCCR1A |= (1<<WGM11);
+	TCCR1B |= (1<<WGM13) | (1<<WGM12) | (1<<CS11) | (1<<CS10);
+	ICR1 = 4999;
+	TCCR1A |= (1<<COM1A1)  | (1<<COM1B1) ;
+	OCR1A = 0;
+	OCR1B = 0;
+			
     while (1){
 		
     }
