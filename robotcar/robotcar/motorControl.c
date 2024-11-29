7efbd219276ea53d9ba9867156a6b48a8d86da9d
Taco-san <143967870+Taco-san@users.noreply.github.com>
2024-11-29 11:58:15 +0100

    Update motorControl.c

--- robotcar/robotcar/motorControl.c
+++ robotcar/robotcar/motorControl.c
@@ -9,12 +9,12 @@
 #include <util/delay.h>
 
 // Snellheid gaat van 0 naar 4999 0 stillstand 4999 max
-// Motor forward functie voor snellheid aanpassen integers van 0 -> 4999 maximaalmotorForward(int speed1, int speed2){	PORTB = 0b00000110;
+// Motor forward functie voor snellheid aanpassen integers van 0 -> 4999 maximaalvoid motorForward(int speed1, int speed2){	PORTB = 0b00000110;
 	PORTD = 0b00000000;	OCR1A = speed1;
-	OCR1B = speed2;}// Motor backward functie voor snellheid aanpassen integers van 0 -> 4999 maximaalmotorBackward(int speed1, int speed2){	PORTB = 0b00000111;
+	OCR1B = speed2;}// Motor backward functie voor snellheid aanpassen integers van 0 -> 4999 maximaalvoid motorBackward(int speed1, int speed2){	PORTB = 0b00000111;
 	PORTD = 0b10000000;	OCR1A = 4999 - speed1;
-	OCR1B = 4999 - speed2;}// Tank draai functie voor snellheid aanpassen integers van 0 -> 4999 maximaaltankDraaiLinks(int speed1, int speed2){	PORTB = 0b00000110;
+	OCR1B = 4999 - speed2;}// Tank draai functie voor snellheid aanpassen integers van 0 -> 4999 maximaalvoid tankDraaiLinks(int speed1, int speed2){	PORTB = 0b00000110;
 	PORTD = 0b10000000;	OCR1A = 4999 - speed1;
-	OCR1B = speed2;}// Tank draai functie voor snellheid aanpassen integers van 0 -> 4999 maximaaltankDraaiRechts(int speed1, int speed2){	PORTB = 0b00000111;
+	OCR1B = speed2;}// Tank draai functie voor snellheid aanpassen integers van 0 -> 4999 maximaalvoid tankDraaiRechts(int speed1, int speed2){	PORTB = 0b00000111;
 	PORTD = 0b00000000;	OCR1A = speed1;
 	OCR1B = 4999 - speed2;}
