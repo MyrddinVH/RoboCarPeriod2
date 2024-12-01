b76da7c896057fbcca1b6061173e1360795b4b57
Taco-san <143967870+Taco-san@users.noreply.github.com>
2024-12-01 22:13:39 +0100

    Motor control verranderd naar bitwise operation
    Void weggehaald zonder krijg je namelijk geen error checken op school

--- robotcar/robotcar/motorControl.c
+++ robotcar/robotcar/motorControl.c
@@ -9,12 +9,12 @@
 #include <util/delay.h>
 
 // Snellheid gaat van 0 naar 4999 0 stillstand 4999 max
-// Motor forward functie voor snellheid aanpassen integers van 0 -> 4999 maximaalvoid motorForward(int speed1, int speed2){	PORTB |= 0b00000110;
-	PORTD |= 0b00000000;	OCR1A = speed1;
-	OCR1B = speed2;}// Motor backward functie voor snellheid aanpassen integers van 0 -> 4999 maximaalvoid motorBackward(int speed1, int speed2){	PORTB |= 0b00000111;
-	PORTD |= 0b10000000;	OCR1A = 4999 - speed1;
-	OCR1B = 4999 - speed2;}// Tank draai functie voor snellheid aanpassen integers van 0 -> 4999 maximaalvoid tankTurnLeft(int speed1, int speed2){	PORTB |= 0b00000110;
-	PORTD |= 0b10000000;	OCR1A = 4999 - speed1;
-	OCR1B = speed2;}// Tank draai functie voor snellheid aanpassen integers van 0 -> 4999 maximaalvoid tankTurnRight(int speed1, int speed2){	PORTB |= 0b00000111;
-	PORTD |= 0b00000000;	OCR1A = speed1;
+// Motor forward functie voor snellheid aanpassen integers van 0 -> 4999 maximaalmotorForward(int speed1, int speed2){	PORTB &= ~(1<<PORTB0); // PortB bit 0 uitzetten
+	PORTD &= ~(1<<PORTD7); // PortB bit 7 uitzetten	OCR1A = speed1;
+	OCR1B = speed2;}// Motor backward functie voor snellheid aanpassen integers van 0 -> 4999 maximaalmotorBackward(int speed1, int speed2){	PORTB |= (1<<PORTB0); // PortB bit 0 aanzetten
+	PORTD |= (1<<PORTD7); // PortB bit 7 aanzetten	OCR1A = 4999 - speed1;
+	OCR1B = 4999 - speed2;}// Tank draai functie voor snellheid aanpassen integers van 0 -> 4999 maximaaltankTurnLeft(int speed1, int speed2){	PORTB &= ~(1<<PORTB0);
+	PORTD |= (1<<PORTD7);	OCR1A = 4999 - speed1;
+	OCR1B = speed2;}// Tank draai functie voor snellheid aanpassen integers van 0 -> 4999 maximaaltankTurnRight(int speed1, int speed2){	PORTB |= (1<<PORTB0);
+	PORTD &= ~(1<<PORTD7);	OCR1A = speed1;
 	OCR1B = 4999 - speed2;}
