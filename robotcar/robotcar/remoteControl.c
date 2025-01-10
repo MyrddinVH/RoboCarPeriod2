9156994861d6a34aac6cbbb44b042e724eff80dc
Huub <HJ.Bouwman@student.han.nl>
2025-01-10 13:12:00 +0100

    2 rare errors, maar de soft serial zit erin

--- robotcar/robotcar/remoteControl.c
+++ robotcar/robotcar/remoteControl.c
@@ -6,8 +6,48 @@
  */ 
 #define F_CPU 16000000UL
 #include <avr/io.h>
-#include "motorControl.h"
+#include <avr/interrupt.h>
+#include <SoftSerial.h>
+#include <Debugger.h>
 
-void remoteControl(){
-	motorBackward(100, 100);
+void remoteControl()
+{
+    DDRB = (1<<DDB5);
+
+    SoftSerialInit();
+    usart0_init();
+    sei();
+
+    SoftSerialTransmitString("\r\nSoftSerial demo\r\n");
+
+    while (1)
+    {
+	    if(SoftSerialUnread() > 0)
+	    {
+		    char data = SoftSerialReceiveByte();
+
+		    switch( data )
+		    {
+
+			    case '1':
+			    //PINB = (1<<PINB5);
+			    //PINB |= (1<<PINB0);
+			    usart0_transmitStr("it workss\n");
+			    break;
+
+			    case 'h':
+			    SoftSerialTransmitString("hello world!\r\n");
+			    break;
+
+			    case 'H':
+			    SoftSerialTransmitString("HELLO WORLD!\r\n");
+			    break;
+
+			    default:
+			    SoftSerialTransmitString("Unknown command\r\n");
+			    break;
+		    }
+
+	    }
+    }
 }
